3dc9cc94ea8a97619d8175e63cf1c68461accdbc
ENZO HENRIQUE PANDOLFO CALLEMI <enzo.hpcallemi@senacsp.edu.br>
2024-08-22 09:49:51 -0300

    aula javascript 1

---
+++ index.js
@@ -0,0 +1,25 @@
+
+//variaveis
+//let nome = "tom"
+//let idade = 15
+//let altura =  1.50
+//let estaChovento = false
+
+//fazendo pergunta para o usuario
+//let nome = prompt ("Digite o seu nome:")
+
+//alert("Oi!") //frontend
+//console.log("Oi" , nome,"Suavão?","qual sua idade?",idade,"pdp viado, qual sua altura?",altura) //backend
+
+
+
+let pergunta = parseFloat(prompt("Qual foi o resultado da primeira prova?:"));
+let pergunta2 = parseFloat(prompt("E da segunda prova?"));
+let pergunta3 = parseFloat(prompt("Última prova?"));
+
+let media = (pergunta + pergunta2 + pergunta3) / 3;
+
+alert("Seu resultado final é: 5.10")
+
+
+
